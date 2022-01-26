973501fa02d50d1ee352ae0431cd944417d66824
Mike_Bezhaneishvili <56588600+igggiRUS@users.noreply.github.com>
2022-01-27 00:57:30 +0300

    незнаю что творю но надо пробовать

--- VideoCourseOracle/src/lesson04/Car.java
+++ VideoCourseOracle/src/lesson04/Car.java
@@ -1,10 +1,19 @@
 package lesson04;
 public class Car {
 
-    Car (){  // метод в теле конструктора
-    Car(String color, String engine)
-    } // дефолтный конструктор создается компилятором мы его не пишем
-    System.out.println("цвет: " + color + " двигатель: " + engine);
-
+    // в теле конструктора метод
+    Car(String cvet, String motor);
+                {
+                    color = cvet;
+                    engine = motor;
+                    System.out.println("цвет: " + color + " двигатель: " + engine);
+                }
+    String color;
+    String engine;
 }
 
+class CarTest05 {
+    public static void main(String[] args) {
+        Car car1 =new Car("black", "v6");
+    }
+}
