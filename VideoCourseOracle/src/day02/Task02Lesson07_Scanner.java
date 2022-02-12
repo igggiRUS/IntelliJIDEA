1d7106b0e1ff80d99984f26e0f5a23608b62981a
Mike_Bezhaneishvili <56588600+igggiRUS@users.noreply.github.com>
2022-02-12 18:38:07 +0300

    заново написал файл сканера

---
+++ VideoCourseOracle/src/day02/Task02Lesson07_Scanner.java
@@ -0,0 +1,13 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Task02Lesson07_Scanner {
+
+  public static void main(String[] args) {
+    Scanner s1 = new Scanner(System.in);
+    System.out.println("input");
+    String stringWord = s1.nextLine();
+    System.out.println("u are input " +stringWord);
+  }
+}
