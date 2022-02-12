1079c0da5bdd0f67ced4e223ded8912e8e31aad6
Mike_Bezhaneishvili <56588600+igggiRUS@users.noreply.github.com>
2022-02-12 19:16:48 +0300

    сделал задачу с небоскребами день 2 урок 6 и 7

--- VideoCourseOracle/src/day02/Task01Home_if.java
+++ VideoCourseOracle/src/day02/Task01Home_if.java
@@ -1,22 +1,26 @@
 package day02;
 
+import java.util.Scanner;
+
 public class Task01Home_if {
+
   public static void main(String[] args) {
+    System.out.println("Введите этажность дома");
+    Scanner newIntInput = new Scanner(System.in);
     /*
-    если денег меньше то 1.... иначе если то 2 ....
+    если  то 1.... иначе если то 2 ....
     иначе если то 3... иначе (во всех остальных случайях)...
     */
-    double money = 15000.0; // Количество денег до зарплаты
-    System.out.println(money +" Количество денег до зарплаты");
-    if (money < 3000) {
-      System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-    } else if (money < 10000) {
-      System.out.println("Окей, пора в Макдак!");
-      // вприсал условие  AND && логический оператор сравнения
-    } else if (money > 10000 && money < 30000) {
-      System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место.");
+    int floorCount = newIntInput.nextInt(); // Количество этажей
+    if (floorCount < 5) {
+      System.out.println(floorCount + " С 1 по 4 малоэтажный");
+    } else if (floorCount >= 5 && floorCount <= 8) {
+      System.out.println("С 5 по 8 Средне этажный");
+      // вписал условие  AND && логический оператор сравнения
+    } else if (floorCount >= 9 && floorCount <= 16) {
+      System.out.println("Многоэтажный");
     } else {
-      System.out.println("Класс! Заказывайте крабов!");
+      System.out.println("Небоскреб");
     }
   }
 }
