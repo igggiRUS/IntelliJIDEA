c93ff59c43e424f8b3df70e45d4bd1bd36e054d0
Mike_Bezhaneishvili <56588600+igggiRUS@users.noreply.github.com>
2022-02-13 13:34:08 +0300

    Поменял задачки и нумерацию задач что бы они соответствовали pdf файлам

--- VideoCourseOracle/src/day02/Task01Home07_for.java
+++ VideoCourseOracle/src/day02/Task01Home07_for.java
@@ -1,23 +1,21 @@
 package day02;
 
-import static java.lang.System.*;
-
 import java.util.Scanner;
 
 // удобнее читать аналог файла Task01Home02_if
-public class Task01Home03_for {
+public class Task01Home07_for {
 
   public static void main(String[] args) {
-    int a = 7;
-    int b = 78;
-    /*
-    int a1 = a.nextInt();
-    int b1 = b.nextInt();
-    */
+    System.out.println("Введите число 7 и 78 через энтер Программа найдет все что делится на 5");
+    Scanner a1 = new Scanner(System.in);
+    Scanner b1 = new Scanner(System.in);
+    int a = a1.nextInt();
+    int b = b1.nextInt();
+
     for (int i = a++; i < b; i++) {
       // сравнивает деление на 5 и 10 если сходится то пишет вывод
       if (i % 5 == 0 && i % 10 != 0) {
-        out.println(i + " ");
+        System.out.println(i + " ");
       }
     }
   }
--- VideoCourseOracle/src/day02/Task02Lesson07_Scanner.java
+++ VideoCourseOracle/src/day02/Task02Lesson07_Scanner.java
@@ -6,8 +6,8 @@ public class Task02Lesson07_Scanner {
 
   public static void main(String[] args) {
     Scanner s1 = new Scanner(System.in);
-    System.out.println("input");
+    System.out.println("Введите любой текст или значение");
     String stringWord = s1.nextLine();
-    System.out.println("u are input " +stringWord);
+    System.out.println("вы ввели " +stringWord);
   }
 }
