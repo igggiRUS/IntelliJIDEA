bc551241d572ccee95d48b0149c3e5ad35bd73ef
Mike_Bezhaneishvili <56588600+igggiRUS@users.noreply.github.com>
2022-02-20 23:21:23 +0300

    начал делать Милкину она проще

---
+++ Milkina/src/lesson05/lesson05_00_review.java
@@ -0,0 +1,27 @@
+package lesson05;
+
+import java.util.Scanner;
+
+public class lesson05_00_review {
+  /*
+  Ввод со сканера
+  Проверяется условие ввели ли целое число if (условие)
+  scanner.hasNextInt
+  Если число целое то в теле выполняется метод
+  if (условие) {
+  метод
+  }
+  */
+
+
+  public static void main(String[] args) {
+    System.out.println("Введите число");
+    Scanner scanner = new Scanner(System.in);
+    if (scanner.hasNextInt()) {
+      int i = scanner.nextInt();
+      System.out.println(i);
+    } else {
+      System.out.println("Вы ввели не целое чиcло");
+    }
+  }
+}
---
+++ Milkina/src/lesson05/lesson05_01_chet_nechet.java
@@ -0,0 +1,16 @@
+package lesson05;
+
+public class lesson05_01_chet_nechet {
+
+  public static void main(String[] args) {
+String y = "33";
+    int x = Integer.parseInt(y);
+    System.out.println("вы ввели строку преобразованную в число: " + x);
+     if (x % 2 == 0) {
+       System.out.println("Число четное: " + x);
+       } else {
+       System.out.println("Число не четное: " + x);
+     }
+  }
+}
+
